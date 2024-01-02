61d7389e75ecf9f1a0e61fa832adcb1a015cca54
root <anikgtx@gmail.com>
2024-01-02 07:22:00 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-nx build news-site --prod
-rsync -arvz --delete dist/apps/news-site/* root@159.89.181.241:/var/www/web
-#ssh root@159.89.181.241 'cd /var/www/web && pm2 kill && pm2 start npm -- start'
+cd dist/apps/news-site
+PORT=4200 pm2 start npm --name "next-js:web" -- start
