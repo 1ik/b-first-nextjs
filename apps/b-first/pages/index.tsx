8305373bf2b4b3937bac36ecc70e7c6f173e58ce
Anik Hasan <anikgtx@gmail.com>
2024-02-01 13:19:00 +0600

    WIP

--- apps/b-first/pages/index.tsx
+++ apps/b-first/pages/index.tsx
@@ -852,7 +852,11 @@ export function CardCaption(props: { item: any }) {
   return (
     <div className="hover-img bg-gray-100">
       <a href="">
-        <img className="max-w-full w-full mx-auto" src={getImageUrl(item.featured_image)} alt="alt title" />
+        <img
+          className="max-w-full w-full mx-auto"
+          src={getImageUrl(item.featured_image, 1600, 900)}
+          alt={item.featured_image_caption}
+        />
       </a>
       <div className="py-3 px-6">
         <span className="font-black font-semibold">{item.featured_image_caption}</span>
