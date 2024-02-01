cbe122ed6af43aa88372c5b9d9f44851b1b73cc4
Anik Hasan <anikgtx@gmail.com>
2024-02-01 13:27:46 +0600

    WIP

--- libs/news-site-ui/src/lib/image_utils.ts
+++ libs/news-site-ui/src/lib/image_utils.ts
@@ -1,9 +1,9 @@
 export const getImageUrl = (path: string, w?: number, h?: number) => {
   if (!w) {
-    w = 300;
+    w = 1600;
   }
   if (!h) {
-    h = 200;
+    h = 900;
   }
   return `https://imaginary.bangladeshfirst.com/crop?width=${w}&height=${h}&quality=80&type=webp&path=${path}`;
 };
