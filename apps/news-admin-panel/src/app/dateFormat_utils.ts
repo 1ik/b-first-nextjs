eeb68f6046dc120f126ec801437415dbc973d8ea
mahbubalamsiddik <mahbubalamsiddik97@gmail.com>
2024-03-13 11:11:14 +0600

    manage feature page modified

---
+++ apps/news-admin-panel/src/app/dateFormat_utils.ts
@@ -0,0 +1,13 @@
+export const dateFormatter = (dateString: string) => {
+    const date = new Date(dateString);
+
+    const formattedDate = new Intl.DateTimeFormat("en-US", {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    }).format(date);
+    return formattedDate.replace(/,/g, "");
+  };
