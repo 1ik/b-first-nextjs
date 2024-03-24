a633b5c037499ffb8bd3bd14bdc21e2e24028a92
Hasan <thehasandev@gmail.com>
2024-03-24 10:05:35 +0600

    refector stories list

---
+++ libs/features/stories/src/lib/stories_list.tsx
@@ -0,0 +1,84 @@
+import { useGet } from "@bfirst/api-client";
+import { Table, TableColumnDef } from "@bfirst/components-table";
+import { Icon } from "@bfirst/components-icon";
+import { Typography } from "@bfirst/material-tailwind";
+import moment from "moment";
+import { useState } from "react";
+
+export function StoriesList() {
+  const TABLE_COLUMNS: TableColumnDef[] = [
+    {
+      key: "id",
+      colKey: "id",
+      title: "ID",
+      width: "10%",
+    },
+    {
+      key: "title",
+      colKey: "title",
+      title: "Title",
+      width: "50%",
+    },
+    {
+      key: "createdAt",
+      colKey: "created_at",
+      title: "Created At",
+      width: "20%",
+      render: (row) => {
+        return (
+          <Typography variant="small" className="font-normal leading-none opacity-70">
+            {moment(row["created_at"]).format("YYYY-MM-DD hh:mm a")}
+          </Typography>
+        );
+      },
+    },
+    {
+      key: "updatedAt",
+      colKey: "updated_at",
+      title: "Updated At",
+      width: "20%",
+      render: (row) => {
+        return (
+          <Typography variant="small" className="font-normal leading-none opacity-70">
+            {moment(row["updated_at"]).format("YYYY-MM-DD hh:mm a")}
+          </Typography>
+        );
+      },
+    },
+    {
+      key: "action",
+      colKey: "id",
+      title: "Action",
+      width: "30%",
+      className: "text-right",
+      render: (row) => {
+        return (
+          <div className="flex items-end gap-4 justify-end w-full">
+            <Icon name="trash" />
+            <Icon name="pencil" />
+          </div>
+        );
+      },
+    },
+  ];
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data } = useGet(`api/v1/stories?page=${currentPage}&size=20`);
+
+  if (!data) {
+    return <></>;
+  }
+
+  return (
+    <Table
+      columns={TABLE_COLUMNS}
+      data={data.data}
+      pagination={{
+        currentPage,
+        pageChanged: (page: number) => {
+          setCurrentPage(page);
+        },
+      }}
+    />
+  );
+}
