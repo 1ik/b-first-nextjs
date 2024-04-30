1969c046b42b5677711365403e36d215f60a2cbc
Hasan <thehasandev@gmail.com>
2024-04-30 16:57:18 +0600

    added accent header color

--- libs/components/accent-header/src/lib/accent-header.tsx
+++ libs/components/accent-header/src/lib/accent-header.tsx
@@ -5,10 +5,8 @@ export interface AccentHeaderProps {
 
 export function AccentHeader({ header, color }: AccentHeaderProps) {
   return (
-    <div className={`border-b border-${color}`}>
-      <h3
-        className={`bg-${color} inline px-4 py-1 font-montserrat font-bold text-white text-[22px] border-b border-${color}`}
-      >
+    <div className={`border-b border-[${color}]`}>
+      <h3 className={`bg-[${color}] inline px-4 py-1 font-montserrat font-bold text-white text-[22px]`}>
         {header}
       </h3>
     </div>
--- libs/components/itemCardHorizontal/src/lib/itemCardHorizontal.tsx
+++ libs/components/itemCardHorizontal/src/lib/itemCardHorizontal.tsx
@@ -20,7 +20,7 @@ export function ItemCardHorizontal({
 }: ItemCardHorizontalProps) {
   return (
     <div className={`${itemsWidth}`}>
-      <AccentHeader  header="Bangladesh" color="red-500"/>
+      <AccentHeader  header="Bangladesh" color={"red"}/>
       <div className={`flex justify-between mt-5 ${flexDirection}`}>
         <div className={itemLeftWidth}>
           <h2
