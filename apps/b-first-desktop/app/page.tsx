c0af30f7e87ad3c43d6afa63304dc33dcc117436
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-04-30 12:41:39 +0600

    reconfigured container

--- apps/b-first-desktop/app/page.tsx
+++ apps/b-first-desktop/app/page.tsx
@@ -1,9 +1,3 @@
-import Container from "./components/Container/Container";
-
 export default function Index() {
-  return (
-    <Container className="">
-      <div className="text-2xl">Index Page</div>
-    </Container>
-  );
+  return <div className="desktop-container bg-red-500">Index page</div>;
 }
