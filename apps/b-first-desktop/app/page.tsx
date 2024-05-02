6462e264bfc876c13516c78deed72be38ae65ccf
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-05-02 19:20:22 +0600

    layout build v1

--- apps/b-first-desktop/app/page.tsx
+++ apps/b-first-desktop/app/page.tsx
@@ -1,15 +1,19 @@
 import { TopNewsSection } from "@bfirst/components-top-news-section";
+import Navbar from "./components/Navbar/Navbar";
 import { getData } from "./utils/dataFetch";
 
 export default async function Index() {
   const data = await getData("categories/0/featured-stories");
   return (
-    <div className="desktop-container">
-      <TopNewsSection
-        data={data.data}
-        ads1="https://placehold.co/320x250?text=Ads"
-        ads2="https://placehold.co/320x250?text=Ads"
-      />
-    </div>
+    <>
+      <Navbar />
+      <div className="desktop-container">
+        <TopNewsSection
+          data={data.data}
+          ads1="https://placehold.co/320x250?text=Ads"
+          ads2="https://placehold.co/320x250?text=Ads"
+        />
+      </div>
+    </>
   );
 }
