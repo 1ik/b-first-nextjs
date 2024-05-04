73a1f5edf26fff8a143954051d77678a37bafdfa
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-05-04 14:48:07 +0600

    item-list modified

--- libs/features/desktop/block-news-3/src/lib/block-news-3.tsx
+++ libs/features/desktop/block-news-3/src/lib/block-news-3.tsx
@@ -1,3 +1,4 @@
+import { AccentHeader } from "@bfirst/components-accent-header";
 import { ItemCardVertical } from "@bfirst/components-item-card-vertical";
 import { ItemList } from "@bfirst/components-item-list";
 
@@ -13,7 +14,8 @@ export function BlockNews3({ data, ads1, ads2 }: BlockNews3Props) {
     <div>
       <div className="grid grid-cols-4">
         <div className="col-span-3 border-r pr-4 mr-4">
-          <div className="border-b mb-3">
+          <AccentHeader header={data[0].categories[0].name} color={data[0].categories[0].color_code} />
+          <div className="border-b mb-3 mt-3">
             <ItemCardVertical data={data[0]} size="lg" titlePosition="inset" />
           </div>
           <div className="grid grid-cols-3">
@@ -24,7 +26,10 @@ export function BlockNews3({ data, ads1, ads2 }: BlockNews3Props) {
           </div>
         </div>
         <div>
-          <ItemList data={data.slice(7, 11)} />
+          <div className="mb-3">
+            <AccentHeader header="On this day" color="#A49A46" />
+          </div>
+          <ItemList data={data.slice(7, 11)} showImage showDate />
           <div className="flex flex-col items-center gap-y-2">
             <img src={ads1} alt="Ads" />
             <img src={ads2} alt="Ads" />
