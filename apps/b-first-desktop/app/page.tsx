46e2e3f2f358cb1f1fd40458a5bfa0388558896f
Hasan <thehasandev@gmail.com>
2024-05-05 18:17:27 +0600

    added ads

--- apps/b-first-desktop/app/page.tsx
+++ apps/b-first-desktop/app/page.tsx
@@ -33,12 +33,12 @@ export default async function Index() {
       <BlockNews
         className="desktop-container"
         data={topNews?.data}
-        ads1="https://placehold.co/320x250?text=Ads"
-        ads2="https://placehold.co/320x250?text=Ads"
+        ads1="/ads/Global.gif"
+        ads2="/ads/SIBL_Profit_300x250.gif"
       />
-      <img className="mx-auto my-12" src="https://placehold.co/720x100?text=Ads" alt="Ads" />
+      <img className="mx-auto my-14" src="/ads/nagad.png" alt="Ads" />
       <SquareGrid showAccentHeader className="desktop-container" data={latestNews?.data.slice(7, 15)} />
-      <img className="mx-auto my-12" src="https://placehold.co/720x100?text=Ads" alt="Ads" />
+      <img className="mx-auto my-12" src="/ads/ads_top.png" alt="Ads" />
 
       <div className="bg-[#F6EFEF] dark:bg-dark-300 py-8">
         <div className="desktop-container">
@@ -46,56 +46,61 @@ export default async function Index() {
           <SquareGrid data={latestNews?.data.slice(8, 12)} />
         </div>
       </div>
-      <img className="mx-auto my-12" src="https://placehold.co/720x100?text=Ads" alt="Ads" />
+      <img className="mx-auto my-12" src="/ads/bkash.png" alt="Ads" />
 
       <div className="desktop-container">
         <div className="grid grid-cols-4">
-          <BlockNews2 adsUrl="https://placehold.co/720x100?text=Ads" className="col-span-3 border-r pr-4 mr-4" data={economyNews?.data} />
+          <BlockNews2
+            adsUrl="/ads/social_islami.png"
+            className="col-span-3 border-r pr-4 mr-4"
+            data={economyNews?.data}
+          />
 
           <div>
             <AccentHeader header="Latest News" color="#5D26D1" />
             <ItemList data={latestNews?.data.slice(0, 6)} listType="circle" />
-            <img className="mt-4 mx-auto" src="https://placehold.co/320x250?text=Ads" alt="Ads" />
+            <img className="mt-4 mx-auto" src="/ads/Global.gif" alt="Ads" />
           </div>
         </div>
       </div>
 
-     
-
       <div className="desktop-container my-10">
         <div className="grid grid-cols-4">
           <div className="col-span-3 border-r pr-4 mr-4">
-            <BlockNews3 adsUrl="https://placehold.co/740x90?text=Ads" data={featureNews?.data} />
-       
+            <BlockNews3 adsUrl="/ads/ads_three.png" data={featureNews?.data} />
           </div>
           <div>
             <AccentHeader header="Most Viewed" color="#119F9F" />
             <ItemList data={latestNews?.data.slice(14, 22)} listType="number" />
-            <img className="mt-4 mx-auto" src="https://placehold.co/320x250?text=Ads" alt="Ads" />
+            <img className="mt-4 mx-auto" src="/ads/Global.gif" alt="Ads" />
           </div>
         </div>
       </div>
 
       <div className="desktop-container">
         <div className="grid grid-cols-4">
-          <BlockNews4 adsUrl="https://placehold.co/720x100?text=Ads" className="col-span-3 border-r pr-4 mr-4" data={latestNews?.data} />
+          <BlockNews4
+            adsUrl="/ads/social_islami.png"
+            className="col-span-3 border-r pr-4 mr-4"
+            data={latestNews?.data}
+          />
           <div>
             <AccentHeader header="On this day" color="#A49A46" />
             <ItemList showImage showDate data={latestNews?.data.slice(14, 19)} />
             <div className="flex flex-col gap-y-3 items-center">
-              <img src="https://placehold.co/320x250?text=Ads" alt="Ads" />
-              <img src="https://placehold.co/320x250?text=Ads" alt="Ads" />
+              <img src="ads/SIBL_Profit_300x250.gif" alt="Ads" />
+              <img src="ads/Global.gif" alt="Ads" />
             </div>
           </div>
         </div>
       </div>
-     
+
       <BlockNews5 className="desktop-container my-10" data={entertainmentNews?.data} />
-      <img className="mx-auto my-16" src="https://placehold.co/720x100?text=Ads" alt="Ads" />
+      <img className="mx-auto my-16" src="/ads/ads_six.png" alt="Ads" />
 
       <ListGrid className="desktop-container" data={listData} />
 
-      <img className="mx-auto my-12" src="https://placehold.co/720x100?text=Ads" alt="Ads" />
+      <img className="mx-auto my-12" src="/ads/fresh.png" alt="Ads" />
 
       <div className="bg-black">
         <Footer className="desktop-container" logo="/img/logo-light.svg" />
