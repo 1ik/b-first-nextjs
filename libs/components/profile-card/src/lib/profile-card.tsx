b53a008bb9c41cb550767caf3468eb2260f4a089
Hasan <thehasandev@gmail.com>
2024-05-08 16:40:40 +0600

    remove card rounded

--- libs/components/profile-card/src/lib/profile-card.tsx
+++ libs/components/profile-card/src/lib/profile-card.tsx
@@ -5,7 +5,7 @@ export interface ProfileCardProps {
 
 export function ProfileCard({ data, updatedTime, }: ProfileCardProps) {
   return (
-    <div className="w-[420px] bg-[#F2F2F2] p-8 rounded-xl">
+    <div className="w-[420px] bg-[#F2F2F2] p-8">
       <img className="h-[120px] w-[120px] rounded-full object-center" src={"https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg"} alt="profile" />
       <h3 className="text-2xl mt-4 mb-3">{`By ${data.name}`}</h3>
       <p className="font-montserrat text-lg">{`Updated ${updatedTime}`}</p>
