4879a0e54e6cdd80c0f555ff4a1c143e27cb2b81
mahbubmafi <mahbubalamsiddik97@gmail.com>
2024-05-26 17:44:50 +0600

    size declear in top news api

--- apps/b-first-mobile/app/page.tsx
+++ apps/b-first-mobile/app/page.tsx
@@ -15,7 +15,7 @@ import filterOutOTD from "./utils/filterOutOTD";
 
 export default async function Index() {
   const [topNews, recommendedNews] = (
-    await Promise.all([getData("categories/0/featured-stories"), getData("recommended-stories")])
+    await Promise.all([getData("categories/0/featured-stories?size=13"), getData("recommended-stories")])
   ).map((item) => item.data);
 
   const filterRecommended = function (item: { id: number }) {
