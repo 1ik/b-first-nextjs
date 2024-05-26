ef3abdf390ebdbf6e439697d72242a17ba496e1b
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-05-26 17:37:26 +0600

    changed top news amount

--- apps/news-admin-panel/src/app/internal/manageStories/manageStories.tsx
+++ apps/news-admin-panel/src/app/internal/manageStories/manageStories.tsx
@@ -1,4 +1,5 @@
 import { useGet, usePost } from "@bfirst/api-client";
+import { Loader } from "@bfirst/components-loader";
 import { TypeAheadSearch } from "@bfirst/components-type-ahead-search";
 import { useEffect, useState } from "react";
 import SortableList, { SortableItem } from "react-easy-sort";
@@ -6,7 +7,6 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Breadcrumb } from "../../components";
 import { dateFormatter } from "../../dateFormat_utils";
-import { Loader } from "@bfirst/components-loader";
 
 const dropTarget = (
   <div className="py-3 text-center text-blue-600/50 font-bold border-2 border-dashed border-blue-600/50 rounded-md">
@@ -20,7 +20,7 @@ export function ManageStories() {
   const [search, setSearch] = useState("");
   const [showConfirmModalFor, setShowConfirmModalFor] = useState<number>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
- 
+
   let debounce: string | number | NodeJS.Timeout | undefined;
   const debounceSearch = function (callback: () => void) {
     clearTimeout(debounce);
@@ -31,14 +31,14 @@ export function ManageStories() {
 
   /* API calls */
   const { data, isSuccess: featuredLoadSuccess } = useGet(
-    `api/v1/public/categories/${categoryOption}/featured-stories`
+    `api/v1/public/categories/${categoryOption}/featured-stories?size=13`
   );
   const { data: searchedNews } = useGet(`api/v1/stories?title=${search}`);
   const { request, isSuccess: featuredSaveSuccess } = usePost(`api/v1/featured/stories/create`);
 
   const handleAddFeaturedStories = function (news: any) {
     if (!news) return;
-    if (featuredStories.length >= 11) {
+    if (featuredStories.length >= 13) {
       toast.success("Last item removed and new one added", {
         position: "top-center",
       });
@@ -53,8 +53,8 @@ export function ManageStories() {
 
   const handleSubmit = async function (e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    if (featuredStories.length !== 11) {
-      toast.warning("Total featured story must be 11", {
+    if (featuredStories.length !== 13) {
+      toast.warning("Total featured story must be 13", {
         position: "top-center",
       });
       return;
