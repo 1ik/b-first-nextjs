97aadf822a5fd81f53fe50804a47260c2f51b7ba
Hasan <thehasandev@gmail.com>
2024-05-28 14:28:52 +0600

    ads replace and block news added color

--- libs/features/desktop/block-news/src/lib/block-news.tsx
+++ libs/features/desktop/block-news/src/lib/block-news.tsx
@@ -14,7 +14,7 @@ export interface BlockNewsProps {
 
 export function BlockNews({ data, sectionHeader, ads1, ads2, className, Link }: BlockNewsProps) {
   return (
-    <div className={`flex gap-x-7 ${className}`}>
+    <div className={`flex gap-x-4 ${className}`}>
       <ItemCardVertical Link={Link} className="w-[35%]" showImageBorder data={data?.[0]} size="lg" />
 
       <div className="w-[25%]">
@@ -26,17 +26,23 @@ export function BlockNews({ data, sectionHeader, ads1, ads2, className, Link }: 
         ))}
       </div>
 
-      <div className="w-[20%] flex flex-col gap-y-5">
+      <div className="w-[20%] flex flex-col gap-y-5 bg-[#FFF3ED] dark:bg-dark-400">
         {data?.slice(6, 8).map((item: any, index: Key | null | undefined) => (
-          <div key={index}>
-            <ItemCardVertical data={item} size="md" titleFontSize="26px" />
+          <div key={index} className="border-b dark:border-dark-300 pb-3 last:border-0">
+            <ItemCardVertical className="p-2.5 dark:p-0" data={item} size="md" titleFontSize="26px"/>
           </div>
         ))}
       </div>
 
-      <div className="w-[20%] flex flex-col gap-y-3">
-        <img src={ads1} alt="Ads" />
-        <img src={ads2} alt="Ads" />
+      <div className="w-[20%] flex flex-col gap-y-4 text-center">
+        <div>
+          <p className="mb-2">Advertisement</p>
+          <img src={ads1} alt="Ads" />
+        </div>
+        <div>
+          <p className="mb-2">Advertisement</p>
+          <img src={ads2} alt="Ads" />
+        </div>
       </div>
     </div>
   );
