91913893240d30e5b3348f119a0ccf2a9e523537
Hasan <thehasandev@gmail.com>
2024-05-28 14:48:12 +0600

    ads replecment and added color for mobile and tab

--- libs/features/mobile/block-news-mob/src/lib/block-news-mob.tsx
+++ libs/features/mobile/block-news-mob/src/lib/block-news-mob.tsx
@@ -11,22 +11,25 @@ export interface BlockNewsMobProps {
   className?: string;
 }
 
-export function BlockNewsMob({ data, sectionHeader, ads1, ads2, className }: BlockNewsMobProps) {
+export function BlockNewsMob({ data, ads1, ads2, className }: BlockNewsMobProps) {
   return (
     <div className={`flex flex-col gap-y-10 ${className}`}>
       <div className="grid grid-cols-1 sm:grid-cols-5 gap-4">
         <ItemCardVertical className="sm:col-span-3" showImageBorder data={data?.[0]} size="lg" />
 
-        <div className="sm:col-span-2 gap-4 item-center mx-auto flex flex-col order-1 sm:order-none">
+        <div className="sm:col-span-2 gap-4 item-center mx-auto flex flex-col order-1 sm:order-none bg-[#FFF3ED] dark:bg-dark-400">
           {data?.slice(1, 3).map((item: any, index: Key | null | undefined) => (
-            <div key={index}>
-              <ItemCardVertical data={item} />
+            <div className="border-b dark:border-dark-300 pb-3 last:border-0" key={index}>
+              <ItemCardVertical className="p-2.5" data={item} />
             </div>
           ))}
         </div>
 
         <div className="sm:col-span-3 sm:mr-4">
-          <img className="mx-auto sm:hidden mt-4 mb-6" src={ads1} alt="Ads" />
+          <div className="text-center sm:hidden mt-4 mb-6">
+            <p className="mb-2">Advertisement</p>
+            <img className="mx-auto" src={ads1} alt="Ads" />
+          </div>
           {data?.slice(3, 8).map((item: any, index: number) => (
             <div>
               <div
@@ -37,12 +40,21 @@ export function BlockNewsMob({ data, sectionHeader, ads1, ads2, className }: Blo
               </div>
             </div>
           ))}
-          <img className="mx-auto sm:hidden mt-4 mb-6" src={ads2} alt="Ads" />
+          <div className="text-center sm:hidden mt-4 mb-6">
+            <p className="mb-2">Advertisement</p>
+            <img className="mx-auto" src={ads2} alt="Ads" />
+          </div>
         </div>
 
         <div className="sm:col-span-2 flex-col gap-y-12 mt-6 order-last hidden sm:flex">
-          <img className="mx-auto" src={ads1} alt="Ads" />
-          <img className="mx-auto" src={ads2} alt="Ads" />
+          <div className="mx-auto text-center">
+            <p className="mb-2">Advertisement</p>
+            <img src={ads1} alt="Ads" />
+          </div>
+          <div className="mx-auto text-center">
+            <p className="mb-2">Advertisement</p>
+            <img src={ads2} alt="Ads" />
+          </div>
         </div>
       </div>
     </div>
