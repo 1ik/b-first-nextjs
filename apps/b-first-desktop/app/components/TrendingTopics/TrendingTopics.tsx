7ebc274fba7e793a716c34368dae05538e871b25
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-05-29 12:53:52 +0600

    added montserrat google font

--- apps/b-first-desktop/app/components/TrendingTopics/TrendingTopics.tsx
+++ apps/b-first-desktop/app/components/TrendingTopics/TrendingTopics.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { HighlightChip, HighlightChipProps } from "@bfirst/components-highlight-chip";
+import "../../../../../libs/fonts/montserrat/index.css";
 
 export default function TrendingTopics({ title, items, className }: HighlightChipProps) {
-  return <HighlightChip className={className} title={title} items={items} />;
+  return <HighlightChip className={`montserrat-regular ${className}`} title={title} items={items} />;
 }
--- libs/components/accent-header/src/lib/accent-header.tsx
+++ libs/components/accent-header/src/lib/accent-header.tsx
@@ -1,15 +1,15 @@
 export interface AccentHeaderProps {
   header: string;
   color?: string;
-  className?:string
+  className?: string;
 }
 
-export function AccentHeader({ header, color,className }: AccentHeaderProps) {
+export function AccentHeader({ header, color, className }: AccentHeaderProps) {
   return (
     <div style={{ borderColor: color || "#EB1923" }} className={`border-b mb-4 ${className}`}>
       <h3
         style={{ backgroundColor: color || "#EB1923" }}
-        className={`inline px-2 py-1 font-montserrat font-bold text-white text-base uppercase`}
+        className={`inline px-2 py-1 montserrat-bold text-white text-base uppercase`}
       >
         {header}
       </h3>
