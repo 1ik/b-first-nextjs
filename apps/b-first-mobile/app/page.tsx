3d2721f4d40bb590862319794e4c955f429b191c
mahbubmafi <mahbubalamsiddik97@gmail.com>
2024-06-02 17:03:59 +0600

    top news size changed

--- apps/b-first-mobile/app/page.tsx
+++ apps/b-first-mobile/app/page.tsx
@@ -16,7 +16,7 @@ import filterOutOTD from "./utils/filterOutOTD";
 
 export default async function Index() {
   const [topNews, recommendedNews] = (
-    await Promise.all([getData("categories/0/featured-stories?size=16"), getData("recommended-stories")])
+    await Promise.all([getData("categories/0/featured-stories?size=15"), getData("recommended-stories")])
   ).map((item) => item?.data);
 
   const filterRecommended = function (item: { id: number }) {
