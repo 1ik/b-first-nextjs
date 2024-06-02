21bd8e7b41a9bdf8f228a11e8d978530d885512a
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-06-02 16:43:04 +0600

    removed loading file  and modified not-found file

--- apps/b-first-mobile/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-    return (
-      <div className="h-screen flex items-center justify-center bg-[#F6EFEF] dark:bg-dark-300">
-        <img className="animate-ping w-12" src="/img/logo-mini.png" alt="logo" />
-      </div>
-    );
-  }
-  
