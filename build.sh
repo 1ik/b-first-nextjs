2e3904788a452490b4b0a99394d627dbcb8cf36d
Anik Hasan <anikgtx@gmail.com>
2024-06-06 10:32:48 +0600

    WIP

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 nx build b-first --prod
 cd dist/apps/b-first
-PORT=4200 pm2 start npm --name "next-js:web" -- start
+PORT=4201 pm2 start npm --name "bf_desktop" -- start
--- scripts/build_bf_mobile.sh
+++ scripts/build_bf_mobile.sh
@@ -1,4 +1,4 @@
 nx build b-first-mobile --prod
-rsync -arvz -d dist/apps/b-first-mobile root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-mobile
+rsync -arvz -d dist/apps/b-first-mobile root@128.199.99.46:/var/www/b-first-nextjs/dist/apps
 rsync -arvz -d dist/apps/b-first-mobile/.next/* root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-mobile/.next
 rsync -arvz -d dist/apps/b-first-mobile/.nx-cache/* root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-mobile/.nx-cache
