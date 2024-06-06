efcc2c1e0f4a7bf437a9cd69a49fdaafdedec23f
Anik Hasan <anikgtx@gmail.com>
2024-06-06 16:42:06 +0600

    Update script

--- scripts/build_bf_mobile.sh
+++ scripts/build_bf_mobile.sh
@@ -1,5 +1,5 @@
 nx build b-first-mobile --prod
-cp ../.env ../dist/apps/b-first-mobile
+cp .env dist/apps/b-first-mobile
 rsync -arvz -d dist/apps/b-first-mobile root@128.199.99.46:/var/www/b-first-nextjs/dist/apps
 rsync -arvz -d dist/apps/b-first-mobile/.next/* root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-mobile/.next
 #rsync -arvz -d dist/apps/b-first-mobile/.nx-cache/* root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-mobile/.nx-cache
