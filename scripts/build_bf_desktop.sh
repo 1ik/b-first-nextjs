0d32bffb5c505ae0ab39d7b2a74e4e7d8f6de303
Anik Hasan <anikgtx@gmail.com>
2024-06-06 10:45:42 +0600

    Adds script to build

--- scripts/build_bf_desktop.sh
+++ scripts/build_bf_desktop.sh
@@ -1,4 +1,5 @@
 nx build b-first-desktop --prod
 rsync -arvz -d dist/apps/b-first-desktop root@128.199.99.46:/var/www/b-first-nextjs/dist/apps
 rsync -arvz -d dist/apps/b-first-desktop/.next/* root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-desktop/.next
-rsync -arvz -d dist/apps/b-first-desktop/.nx-cache/* root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-desktop/.nx-cache
+#rsync -arvz -d dist/apps/b-first-desktop/.nx-cache/* root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-desktop/.nx-cache
+ssh root@128.199.99.46 'pm2 restart bf_desktop'
--- scripts/build_bf_mobile.sh
+++ scripts/build_bf_mobile.sh
@@ -1,4 +1,5 @@
 nx build b-first-mobile --prod
 rsync -arvz -d dist/apps/b-first-mobile root@128.199.99.46:/var/www/b-first-nextjs/dist/apps
 rsync -arvz -d dist/apps/b-first-mobile/.next/* root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-mobile/.next
-rsync -arvz -d dist/apps/b-first-mobile/.nx-cache/* root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-mobile/.nx-cache
+#rsync -arvz -d dist/apps/b-first-mobile/.nx-cache/* root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-mobile/.nx-cache
+ssh root@128.199.99.46 'pm2 restart bf_mobile'
