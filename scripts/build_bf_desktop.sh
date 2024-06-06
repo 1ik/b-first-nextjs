e57721831330b0cb3942f0801f43ed92d9167133
mahbubmafi <mahbubalamsiddik97@gmail.com>
2024-06-06 16:48:19 +0600

    script updated for desktop

--- scripts/build_bf_desktop.sh
+++ scripts/build_bf_desktop.sh
@@ -1,5 +1,5 @@
 nx build b-first-desktop --prod
-cp .env dist/apps/b-first-mobile
+cp .env dist/apps/b-first-desktop
 rsync -arvz -d dist/apps/b-first-desktop root@128.199.99.46:/var/www/b-first-nextjs/dist/apps
 rsync -arvz -d dist/apps/b-first-desktop/.next/* root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-desktop/.next
 #rsync -arvz -d dist/apps/b-first-desktop/.nx-cache/* root@128.199.99.46:/var/www/b-first-nextjs/dist/apps/b-first-desktop/.nx-cache
