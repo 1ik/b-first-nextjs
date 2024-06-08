0c5eb4967872647143ef48f7ae3af2a615b61459
Hasan <thehasandev@gmail.com>
2024-06-08 11:58:24 +0600

    modified mergin

--- apps/b-first-mobile/app/(marketing)/contact-us/page.tsx
+++ apps/b-first-mobile/app/(marketing)/contact-us/page.tsx
@@ -14,13 +14,13 @@ export default function ContactUs() {
 
         <ul className="list-disc pl-4 mb-4">
           <li className="text-base mb-2">
-            <span className="font-semibold mr-2">Email :</span>editor@bangladeshfirst.com
+            <span className="font-semibold">Email : </span>editor@bangladeshfirst.com
           </li>
           <li className="text-base mb-2">
-            <span className="font-semibold mr-1">Phone :</span>+880 961 332 2782
+            <span className="font-semibold">Phone : </span>+880 961 332 2782
           </li>
           <li className="text-base mb-2">
-            <span className="font-semibold mr-1">Address :</span>115 Kazi Nazrul Islam Avenue, Level 12, Bangla Motor,
+            <span className="font-semibold">Address : </span>115 Kazi Nazrul Islam Avenue, Level 12, Bangla Motor,
             Dhaka 1000, Bangladesh
           </li>
         </ul>
