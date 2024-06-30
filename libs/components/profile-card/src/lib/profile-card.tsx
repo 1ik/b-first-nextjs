5dbb5e22384d738f2541373cb4a7abea5fd06b78
mahbubmafi <mahbubalamsiddik97@gmail.com>
2024-06-30 15:02:51 +0600

    profile card modification

--- libs/components/profile-card/src/lib/profile-card.tsx
+++ libs/components/profile-card/src/lib/profile-card.tsx
@@ -10,21 +10,21 @@ export interface ProfileCardProps {
 
 export function ProfileCard({ data, createdTime, shareLink, className }: ProfileCardProps) {
   return (
-    <div className={`bg-[#F2F2F2] p-4 dark:bg-dark-300 grid grid-cols-5 gap-x-2 gap-y-6 items-center ${className}`}>
-      <div className="col-span-2">
+    <div className={`bg-[#F2F2F2] p-4 dark:bg-dark-300 gap-x-2 gap-y-6 items-center ${className}`}>
+      {/* <div className="col-span-2">
         <img
           className="h-[100px] w-[100px] rounded-full object-center"
           src="https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg"
           alt="profile"
         />
-      </div>
-      <div className="col-span-3 montserrat-regular">
+      </div> */}
+      <div className="montserrat-regular">
         <h3 className="text-[22px]">{data?.name}</h3>
         <p className="text-sm mt-2 montserrat-regular">{`Publisted at ${moment(createdTime)
           .tz("Asia/Dhaka")
           .format("h:mm A, ddd MMM Do, YYYY")}`}</p>
       </div>
-      <div className="col-span-5 border-t dark:border-dark-300 pt-2">
+      <div className="border-t dark:border-dark-300 pt-2">
         <SocialShare title="Share News" shareLink={shareLink} />
       </div>
     </div>
