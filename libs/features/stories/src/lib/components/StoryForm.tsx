8a060ee05f2fd72c73543ad1c39466d4064bc344
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-07-10 17:11:24 +0600

    responsive issue solved

--- libs/features/stories/src/lib/components/StoryForm.tsx
+++ libs/features/stories/src/lib/components/StoryForm.tsx
@@ -219,7 +219,7 @@ export function StoryForm({ btnLabel, onSubmit, loading, isError, defaultData }:
             </div>
 
             {/* ============= choosing between featured image and featured video ========== */}
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
               <div className="flex flex-col gap-y-4">
                 <Radio
                   label="Featured Image"
