4d23060e680e4a88d3231cfe4872896a4aef07e3
Hasan <thehasandev@gmail.com>
2024-07-13 14:38:21 +0600

    switch slick slider to swiper slider

--- libs/components/item-slide/src/lib/item-slide.tsx
+++ libs/components/item-slide/src/lib/item-slide.tsx
@@ -1,73 +1,52 @@
 "use client";
-import "slick-carousel/slick/slick.css";
 import { RiArrowRightSLine } from "react-icons/ri";
 import { ItemCardVertical } from "@bfirst/components-item-card-vertical";
 import { RiArrowLeftSLine } from "react-icons/ri";
-import Slider from "react-slick";
-import "./item-slide.css"
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation } from "swiper/modules";
 export interface BlockNews5Props {
   data: any;
 }
 export function ItemSlide({ data }: BlockNews5Props) {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
   return (
-    <div className="slider-container p-0 m-0">
-      <Slider {...settings}>
+    <div className="relative">
+      <Swiper
+        slidesPerView={3}
+        navigation={{ nextEl: ".arrow-left", prevEl: ".arrow-right" }}
+        modules={[Navigation]}
+        className="mySwiper"
+        spaceBetween={30}
+        breakpoints={{
+          360: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+        }}
+      >
         {data.map((item: any, index: any) => {
-          return <ItemCardVertical key={index} data={item} size="sm" />;
+          return (
+            <SwiperSlide>
+              <ItemCardVertical key={index} data={item} size="sm" />
+            </SwiperSlide>
+          );
         })}
-      </Slider>
-    </div>
-  );
-}
-
-export function PrevArrow({ onClick }: any) {
-  return (
-    <div className="absolute top-1/3 xl:top-1/2 left-0 xl:-left-10 -translate-y-1/2 cursor-pointer z-10" onClick={onClick}>
-      <button>
+      </Swiper>
+      <button className="arrow-right arrow absolute top-1/3 xl:top-1/2 left-0 xl:-left-8 z-20 -translate-y-1/2">
         <RiArrowLeftSLine size={30} />
       </button>
-    </div>
-  );
-}
-
-export function NextArrow({ onClick }: any) {
-  return (
-    <div className="absolute top-1/3 xl:top-1/2 right-0 xl:-right-10 -translate-y-1/2 cursor-pointer z-10" onClick={onClick}>
-      <button>
+      <button className="arrow-left arrow absolute top-1/3 xl:top-1/2 right-0 xl:-right-8 z-10 -translate-y-1/2">
         <RiArrowRightSLine size={30} />
       </button>
     </div>
