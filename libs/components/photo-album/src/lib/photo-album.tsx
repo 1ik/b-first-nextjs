4ba40046c189e2f9fd1d5957860455226b410534
Hasan <thehasandev@gmail.com>
2024-07-13 17:26:48 +0600

    switch slick slider to swiper slider

--- libs/components/photo-album/src/lib/photo-album.tsx
+++ libs/components/photo-album/src/lib/photo-album.tsx
@@ -1,33 +1,28 @@
 "use client";
-import { getImageUrl, getNewsUrl } from "@bfirst/utilities";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "./photo-album.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
+import { getImageUrl, getNewsUrl } from "@bfirst/utilities";
+import "swiper/css";
+import "swiper/css/pagination";
+
 export interface PhotoAlbumProps {
   data: any;
 }
 
 export function PhotoAlbum({ data }: PhotoAlbumProps) {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    appendDots: (dots: any) => (
-      <div>
-        <ul className="flex absolute bottom-5 left-1/2 -translate-x-1/2 gap-1.5">{dots}</ul>
-      </div>
-    ),
-    customPaging: () => <div className="w-2.5 h-2.5 rounded-full bg-white cursor-pointer"></div>,
-  };
   return (
-    <div>
-      <Slider {...settings}>
-        {data.map((item: any, index: number) => (
+    <Swiper
+      slidesPerView={1}
+      className="mySwiper"
+      spaceBetween={20}
+      pagination={{ clickable: true }}
+      modules={[Autoplay, Navigation, Pagination]}
+      navigation={{ nextEl: ".arrow-left", prevEl: ".arrow-right" }}
+    >
+      {data.map((item: any, index: number) => (
+        <SwiperSlide key={index}>
           <div key={index} className="relative">
             <a href={getNewsUrl(item)}>
               <img
@@ -35,31 +30,17 @@ export function PhotoAlbum({ data }: PhotoAlbumProps) {
                 src={getImageUrl(item.meta.featured_image)}
                 alt={item.title}
               />
-              <div className="absolute top-0 h-full w-full flex text-center text-white px-8 z-50 bg-black/30 pb-10 items-end justify-center"></div>
+              <div className="absolute top-0 h-full w-full flex text-center text-white px-8 z-10 bg-black/30 pb-10 items-end justify-center"></div>
             </a>
           </div>
-        ))}
-      </Slider>
-    </div>
-  );
-}
-
-export function PrevArrow({ onClick }: any) {
-  return (
-    <div className="absolute top-1/2 z-50 -translate-y-1/2" onClick={onClick}>
-      <button className="text-white">
+        </SwiperSlide>
+      ))}
+      <button className="arrow-right arrow absolute top-1/2 -translate-y-1/2 left-0 z-50 text-white">
         <RiArrowLeftSLine size={40} />
       </button>
-    </div>
-  );
-}
-
-export function NextArrow({ onClick }: any) {
-  return (
-    <div className="absolute top-1/2 right-0 z-50 -translate-y-1/2" onClick={onClick}>
-      <button className="text-white">
+      <button className="arrow-left arrow absolute top-1/2 -translate-y-1/2 right-0 z-50 text-white">
         <RiArrowRightSLine size={40} />
       </button>
-    </div>
+    </Swiper>
   );
 }
