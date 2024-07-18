5a61825a8d0ff6478bbb9c3a29e98913ba3cb607
Hasan <thehasandev@gmail.com>
2024-07-18 12:28:17 +0600

    set filter out news details page

--- apps/b-first-desktop/app/(news-pages)/news/[id]/[slug]/page.tsx
+++ apps/b-first-desktop/app/(news-pages)/news/[id]/[slug]/page.tsx
@@ -16,7 +16,7 @@ import ImagePreview from "../../../../components/PreviewImage/PreviewImage";
 import TrendingTopics from "../../../../components/TrendingTopics/TrendingTopics";
 import { getData } from "../../../../utils/dataFetch";
 import { getAdsObj } from "../../../../utils/getAdsObj";
-import filterCategory from "apps/b-first-desktop/app/utils/filterCategory";
+import filterCategory from "../../../../utils/filterCategory";
 export async function generateMetadata({ params }): Promise<Metadata> {
   const data = await getData(`story/details/${params.id}`);
 
@@ -60,8 +60,6 @@ export async function generateMetadata({ params }): Promise<Metadata> {
 export default async function NewsDetails({ params }) {
   const news_link_url = `${process.env.BASE_URL}/news/${params.id}/${params.slug}`;
   const detailsData = await getData(`story/details/${params.id}`);
-  const ads_list = await getData("ads?page=news_details");
-  const ads_obj = getAdsObj(ads_list?.ads);
 
   if (!detailsData) return notFound();
 
@@ -106,24 +104,25 @@ export default async function NewsDetails({ params }) {
     ],
   };
 
-  const [trendingTopics, topNews, categoryNews] = (
+  const [trendingTopics, topNews, categoryNews, latestNews] = (
     await Promise.all([
       getData("trendy-topics"),
       getData("categories/0/featured-stories"),
       getData(`categories/${detailsData?.story.categories[0].name}/stories`),
+      getData("latest/stories?size=30"),
     ])
   ).map((item) => item?.data);
 
-  const filteredLatestNews = filterCategory(
-    (await getData("latest/stories?size=30"))?.data,
-    "On_This_Day",
-    "Video_Gallery",
-    "Photo_Gallery",
-  );
+  const filteredLatestNews = filterCategory(latestNews, "On_This_Day", "Video_Gallery", "Photo_Gallery");
+
   const tagsArr = detailsData?.story.tags.map((tag: { id: any }) => tag.id);
   const relatedNews = (await getData(`related-stories?tags=${tagsArr.join(",")}`))?.data.filter(
     (rN: { id: any }) => rN.id != params.id
   );
+
+  // data for ads
+  const ads_list = await getData("ads?page=news_details");
+  const ads_obj = getAdsObj(ads_list?.ads);
 
   return (
     <>
