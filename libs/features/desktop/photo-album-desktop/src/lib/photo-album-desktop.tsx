b6b97b06c1451e34e226f415cd7b31038260ce71
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-07-31 18:01:46 +0600

    photo_gallery details page style modified

--- libs/features/desktop/photo-album-desktop/src/lib/photo-album-desktop.tsx
+++ libs/features/desktop/photo-album-desktop/src/lib/photo-album-desktop.tsx
@@ -64,23 +64,25 @@ export function PhotoAlbumDesktop({ data }: PhotoAlbumDesktopProps) {
         )}
       </div>
       <div className="my-2 flex items-center justify-between gap-x-6">
-        <p className="montserrat-bold text-sm">
+        <p className="montserrat-bold text-sm py-2">
           {activeIndex + 1} of {images?.length}
         </p>
-        <div className="flex gap-x-0.5 text-xl">
-          <button
-            className="px-4 py-2 rounded-md hover:bg-dark-300/25 disabled:hover:bg-transparent disabled:opacity-50"
-            ref={prevButtonRef}
-          >
-            <FaArrowLeftLong />
-          </button>
-          <button
-            className="px-3 py-2 rounded-md hover:bg-dark-300/25 disabled:hover:bg-transparent disabled:opacity-50"
-            ref={nextButtonRef}
-          >
-            <FaArrowRightLong />
-          </button>
-        </div>
+        {images.length > 1 ? (
+          <div className="flex gap-x-0.5 text-xl">
+            <button
+              className="px-4 py-2 rounded-md hover:bg-dark-300/25 disabled:hover:bg-transparent disabled:opacity-50"
+              ref={prevButtonRef}
+            >
+              <FaArrowLeftLong />
+            </button>
+            <button
+              className="px-3 py-2 rounded-md hover:bg-dark-300/25 disabled:hover:bg-transparent disabled:opacity-50"
+              ref={nextButtonRef}
+            >
+              <FaArrowRightLong />
+            </button>
+          </div>
+        ) : null}
       </div>
       <div>
         <p>{images[activeIndex]?.imageCaption}</p>
